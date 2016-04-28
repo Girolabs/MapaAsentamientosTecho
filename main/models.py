15cf5c94f6d8481ea146af560e37be0c9d669fe9
Diego V <diegoviveros.m@gmail.com>
2016-04-28 17:50:57 -0400

    Se agregaron los modelos

--- main/models.py
+++ main/models.py
@@ -3,3 +3,111 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Relevamiento (models.Model):
+
+	numero_encuesta = models.CharField(max_length = 1000, blank=True)
+	zona = models.CharField(max_length = 1000, blank=True)
+	ciudad = models.CharField(max_length = 1000, blank=True)
+	nombre_encuestador = models.CharField(max_length = 1000, blank=True)
+	cel_encuestador = models.CharField(max_length = 1000, blank=True)
+	asentamiento = models.CharField(max_length = 1000, blank=True)
+	nombre_lider = models.CharField(max_length = 1000, blank=True)
+	cargo = models.CharField(max_length = 1000, blank=True)
+	cel_lider = models.CharField(max_length = 1000, blank=True)
+	p1 = models.CharField(max_length = 1000, blank=True)
+	p2 = models.CharField(max_length = 1000, blank=True)
+	p3 = models.CharField(max_length = 1000, blank=True)
+	p4 = models.CharField(max_length = 1000, blank=True)
+	p5 = models.CharField(max_length = 1000, blank=True)
+	p6_1 = models.CharField(max_length = 1000, blank=True)
+	p6_2 = models.CharField(max_length = 1000, blank=True)
+	p7 = models.CharField(max_length = 1000, blank=True)
+	p8 = models.CharField(max_length = 1000, blank=True)
+	p9 = models.CharField(max_length = 1000, blank=True)
+	p10 = models.CharField(max_length = 1000, blank=True)
+	p11 = models.CharField(max_length = 1000, blank=True)
+	p12 = models.CharField(max_length = 1000, blank=True)
+	p13 = models.CharField(max_length = 1000, blank=True)
+	p14 = models.CharField(max_length = 1000, blank=True)
+	p15 = models.CharField(max_length = 1000, blank=True)
+	p16 = models.CharField(max_length = 1000, blank=True)
+	p17 = models.CharField(max_length = 1000, blank=True)
+	p18 = models.CharField(max_length = 1000, blank=True)
+	p19 = models.CharField(max_length = 1000, blank=True)
+	p20 = models.CharField(max_length = 1000, blank=True)
+	p21 = models.CharField(max_length = 1000, blank=True)
+	p22 = models.CharField(max_length = 1000, blank=True)
+	p23 = models.CharField(max_length = 1000, blank=True)
+	p24 = models.CharField(max_length = 1000, blank=True)
+	p25 = models.CharField(max_length = 1000, blank=True)
+	p26 = models.CharField(max_length = 1000, blank=True)
+	p27 = models.CharField(max_length = 1000, blank=True)
+	p28 = models.CharField(max_length = 1000, blank=True)
+	p29 = models.CharField(max_length = 1000, blank=True)
+	p30 = models.CharField(max_length = 1000, blank=True)
+	p31 = models.CharField(max_length = 1000, blank=True)
+	p32 = models.CharField(max_length = 1000, blank=True)
+	p33 = models.CharField(max_length = 1000, blank=True)
+	P34_1 = models.CharField(max_length = 1000, blank=True)
+	P34_1_1 = models.CharField(max_length = 1000, blank=True)
+	P34_1_2 = models.CharField(max_length = 1000, blank=True)
+	P34_2 = models.CharField(max_length = 1000, blank=True)
+	P34_2_1 = models.CharField(max_length = 1000, blank=True)
+	P34_2_2 = models.CharField(max_length = 1000, blank=True)
+	p35 = models.CharField(max_length = 1000, blank=True)
+	p36 = models.CharField(max_length = 1000, blank=True)
+	p37 = models.CharField(max_length = 1000, blank=True)
+	p38 = models.CharField(max_length = 1000, blank=True)
+	p39 = models.CharField(max_length = 1000, blank=True)
+	p40 = models.CharField(max_length = 1000, blank=True)
+	p41 = models.CharField(max_length = 1000, blank=True)
+	p42 = models.CharField(max_length = 1000, blank=True)
+	p43 = models.CharField(max_length = 1000, blank=True)
+	p44 = models.CharField(max_length = 1000, blank=True)
+	p45 = models.CharField(max_length = 1000, blank=True)
+	p46 = models.CharField(max_length = 1000, blank=True)
+	p47 = models.CharField(max_length = 1000, blank=True)
+	p48 = models.CharField(max_length = 1000, blank=True)
+	p49 = models.CharField(max_length = 1000, blank=True)
+	p50 = models.CharField(max_length = 1000, blank=True)
+	p51 = models.CharField(max_length = 1000, blank=True)
+	p52 = models.CharField(max_length = 1000, blank=True)
+	p53 = models.CharField(max_length = 1000, blank=True)
+	p53_1 = models.CharField(max_length = 1000, blank=True)
+	p53_2 = models.CharField(max_length = 1000, blank=True)
+	p53_3 = models.CharField(max_length = 1000, blank=True)
+	p53_4 = models.CharField(max_length = 1000, blank=True)
+	p53_5 = models.CharField(max_length = 1000, blank=True)
+	p53_6 = models.CharField(max_length = 1000, blank=True)
+	p53_7 = models.CharField(max_length = 1000, blank=True)
+	p53_8 = models.CharField(max_length = 1000, blank=True)
+	p53_9 = models.CharField(max_length = 1000, blank=True)
+	p53_10 = models.CharField(max_length = 1000, blank=True)
+	p53_11 = models.CharField(max_length = 1000, blank=True)
+	p53_12 = models.CharField(max_length = 1000, blank=True)
+	p53_13 = models.CharField(max_length = 1000, blank=True)
+	p53_14 = models.CharField(max_length = 1000, blank=True)
+	p53_15 = models.CharField(max_length = 1000, blank=True)
+	p53_16 = models.CharField(max_length = 1000, blank=True)
+	p54 = models.CharField(max_length = 1000, blank=True)
+	p55 = models.CharField(max_length = 1000, blank=True)
+
+	def __unicode__(self):
+		return  self.Relevamiento.asentamiento
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
