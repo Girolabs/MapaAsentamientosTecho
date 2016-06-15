0e3d4a638334db4c5cbf20fb62cde3e4bdaeb080
rodrigo valdez <rodri.valdez@gmail.com>
2016-06-15 17:17:17 -0400

    serializer

--- main/serializers.py
+++ main/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from models import Asentamiento
+from main.models import Asentamiento
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
