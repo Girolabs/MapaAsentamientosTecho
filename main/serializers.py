0190bedd0673c7f4030b665f12aad644dfe2bdff
rodrigo valdez <rodri.valdez@gmail.com>
2016-06-21 17:17:50 -0400

    speed optimization

--- main/serializers.py
+++ main/serializers.py
@@ -17,4 +17,18 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
 class AsentamientoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Asentamiento
-      #  fields = ('asentamiento', 'ciudad')
+        fields = ('identificador','asentamiento', 'ciudad', 'superficie','poblador_mas_antiguo','numero_familias','numero_aprox_viviendas',
+        			'agua_tipo_provision','energia_tipo_provision','excretas','alumbrado_publico','alumbrado_publico_otro',
+        			'cocina_metodo','cocina_metodo_otro','problema_eliminacion_basura','eliminacion_basura_tipo','comision_vecinal'
+        	)
+
+
+
+class AsentamientoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Asentamiento
+        fields = ('identificador','asentamiento', 'ciudad', 'superficie','poblador_mas_antiguo', 'poblador_mas_nuevo' ,'numero_familias','numero_aprox_viviendas',
+        		'numero_aprox_viviendas_precarias',	'agua_tipo_provision','energia_tipo_provision','excretas','alumbrado_publico','alumbrado_publico_otro',
+        			'cocina_metodo','cocina_metodo_otro','problema_eliminacion_basura','eliminacion_basura_tipo','comision_vecinal','esta_loteada',
+        			'origen', 'numero_lotes','medida_lotes'
+        	)
