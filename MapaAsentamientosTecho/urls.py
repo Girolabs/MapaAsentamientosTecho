4f1801520c2e516276f966c7fce52a17035d143c
lorencab <lorenzo@girolabs.com>
2022-06-27 17:27:13 -0400

    changes in imports

--- MapaAsentamientosTecho/urls.py
+++ MapaAsentamientosTecho/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.urls import include, path
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('main.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
   
 ]
